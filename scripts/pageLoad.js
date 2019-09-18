9f439fcaa10234ace1e696d876ba3f805c862393
dcbears <54871742+dcbears@users.noreply.github.com>
2019-09-17 22:34:41 -0700

    Add files via upload

--- scripts/pageLoad.js
+++ scripts/pageLoad.js
@@ -1,18 +1,19 @@
 $(document).ready( function () {
+    $("#header").load("/includes/header.html");
     highlightActiveNavItem();
   });
 
 function highlightActiveNavItem() {
-    var pageLinks = ["/", "/about/", "/news/", "/links/"];
+    var pageLinks = ["about", "news", "links"];
     var currentSection = window.location.pathname;
     currentLink = ""
     if (currentSection == "" || currentSection == "/" || currentSection == "/index.html"){
         $('a[href="/"]').parent().addClass("current");
         return;
     }
-    for(i=1; i< pageLinks.length; i++) {
-        if(currentSection.startsWith(pageLinks[i])) {
-            $('a[href="'+ pageLinks[i] +'"]').parent().addClass("current");
+    for(i=0; i< pageLinks.length; i++){
+        if (currentSection.startsWith(pageLinks[i])) {
+            $('a[href="/'+pageLinks[i]+'/"]').parent().addClass("current");
             return;
         }
     }

33835bbe23ae832acbca35546e545da96c3efa19
dcbears <54871742+dcbears@users.noreply.github.com>
2019-09-17 21:59:02 -0700

    Add files via upload

--- scripts/pageLoad.js
+++ scripts/pageLoad.js
@@ -1,11 +1,12 @@
-
-var pageLinks = ["/", "/about/", "/news/", "/links/"];
-var currentSection = window.location.pathname;
-currentLink = ""
-for(i=0; i< pageLinks.length; i++) {
-    if(currentSection.startsWith(pageLinks[i])) {
-        currentLink = pageLinks[i];
-        break;
+function pageLoad() {
+    var pageLinks = ["/", "/about/", "/news/", "/links/"];
+    var currentSection = window.location.pathname;
+    currentLink = ""
+    for(i=0; i< pageLinks.length; i++) {
+        if(currentSection.startsWith(pageLinks[i])) {
+            currentLink = pageLinks[i];
+            break;
+        }
     }
+    $('a[href^="'+ currentLink +'"]').parent().addClass("current");
 }
-$('a[href^="'+ currentLink +'"]').parent().addClass("current");

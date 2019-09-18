79f2351992815830d8e5d1c17cabdda33e457f9b
dcbears <54871742+dcbears@users.noreply.github.com>
2019-09-17 21:29:56 -0700

    Add files via upload

---
+++ scripts/pageLoad.js
@@ -0,0 +1,11 @@
+
+var pageLinks = ["/", "/about/", "/news/", "/links/"];
+var currentSection = window.location.pathname;
+alert(currentSection);
+var headerStr = "";
+var currentPageMenuItem = $(a[href=currentSection]).parent();
+alert(currentPageMenuItem);
+var currentClass = $(currentPageMenuItem).attr("class");
+currentClass += " current";
+alert(currentClass);
+$(currentPageMenuItem).attr("class", currentClass)
